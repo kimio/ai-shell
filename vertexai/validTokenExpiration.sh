6c6a7b91a0339b750b7fc5e1e6eccafa77a671cb
Felipe Kimio Nishikaku <kimiokun1@gmail.com>
2025-03-15 09:16:34 -0300

    fix check_valid_token

--- vertexai/validTokenExpiration.sh
+++ vertexai/validTokenExpiration.sh
@@ -2,9 +2,9 @@
 
 check_valid_token() {
   local token_file="$1"
-  local extracted_datetime=$(echo "$token_file" | sed -n 's/..\/config\/google\/access_token_\([0-9]\{4\}-[0-9]\{2\}-[0-9]\{2\} [0-9]\{2\}:[0-9]\{2\}:[0-9]\{2\}\).*/\1/p')
-  local current_time=$(date +"%Y-%m-%d %H:%M:%S")
-  if [[ "$extracted_datetime" > "$current_time" ]]; then
+  local extracted_datetime=$(echo "$token_file" | sed -E 's/[^0-9]//g')
+  local current_time=$(date +"%Y%m%d%H%M%S")
+  if [ "$extracted_datetime" -gt "$current_time" ]; then
     return 0
   fi
   return 1
